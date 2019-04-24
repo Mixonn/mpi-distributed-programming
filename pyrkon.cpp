9415b629d65632339a42b42de0a3428ca6e84e49
Bartosz Osipiuk <osipiuk.bartosz@gmail.com>
2019-04-24 17:42:54 +0200

    Add pthread - 2 loops with semaphores

--- pyrkon.cpp
+++ pyrkon.cpp
@@ -1,29 +1,56 @@
-#include <mpi.h>
+#include <mpi.h> //with openmpi use #include "mpi.h"
 #include <stdio.h>
-#include <semaphore.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
+pthread_mutex_t mutex;
+pthread_mutex_t mutexClock;
+sem_t semaphore;
+
+typedef struct Packet_t {
+    int clock_d;
+    int queueNumber;
+    int direction;
+    int tid;
+    int requestType;
+} Packet;
+
+int clock_d = 0;
+int myTid;
+int world_size;
+
+void* send_loop(void *id){
+    while(1){
+        printf("Hello from func:  processor %d, out of %d processors\n",
+               myTid, world_size);
+        sleep(5);
+    }
+}
 
 int main(int argc, char** argv) {
     // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
+    MPI_Init(&argc, &argv);
 
     // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank (MPI_COMM_WORLD, &myTid);        /* get current process id */
+    MPI_Comm_size (MPI_COMM_WORLD, &world_size);
 
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    pthread_attr_t attr;
+    pthread_attr_init(&attr );
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    pthread_mutex_init (&mutex, nullptr);
+    pthread_mutex_init (&mutexClock, nullptr);
+    sem_init(&semaphore,1,1);
+    sem_wait(&semaphore);
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+    pthread_t threadId;
+    pthread_create(&threadId, &attr, send_loop, nullptr);
 
     // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
+    printf("Hello from main:  processor %d, out of %d processors\n",
+           myTid, world_size);
 
     // Finalize the MPI environment.
     MPI_Finalize();
