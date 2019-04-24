e1e2e6721c5bd49d1fbba076efa836947c7f17ef
Bartosz Osipiuk <osipiuk.bartosz@gmail.com>
2019-04-24 18:22:07 +0200

    Add mpi send and mpi recive

--- pyrkon.cpp
+++ pyrkon.cpp
@@ -1,30 +1,64 @@
 #include <mpi.h> //with openmpi use #include "mpi.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <unistd.h> //for sleep
 #include <pthread.h>
 #include <semaphore.h>
+#include <string>
+#include <iostream>
 
 pthread_mutex_t mutex;
 pthread_mutex_t mutexClock;
 sem_t semaphore;
 
-typedef struct Packet_t {
+class Packet {
+public:
     int clock_d;
-    int queueNumber;
-    int direction;
+    int type;
+    int message;
     int tid;
     int requestType;
-} Packet;
+};
 
 int clock_d = 0;
 int myTid;
 int world_size;
 
+void recive(Packet *packet){
+    MPI_Status status;
+    int vector[5];
+
+    MPI_Status req;
+    MPI_Recv(vector, 5, MPI_INT, MPI_ANY_SOURCE, myTid, MPI_COMM_WORLD, &req);
+
+    pthread_mutex_lock(&mutexClock);
+    if (packet->clock_d > clock_d){
+        clock_d = packet->clock_d;
+    }
+    clock_d++;
+    pthread_mutex_unlock(&mutexClock);
+
+    packet->clock_d = vector[0];
+    packet->type = vector[1];
+    packet->message = vector[2];
+    packet->tid = vector[3];
+    packet->requestType = vector[4];
+}
+
+void send_to_tid(int receiver, int type, int message, int requestType, int setClock){
+    int vector[5] = {setClock, type, message, myTid, requestType};
+    MPI_Request req;
+    MPI_Isend(vector, 5, MPI_INT, receiver, receiver, MPI_COMM_WORLD, &req);
+}
+
 void* send_loop(void *id){
     while(1){
-        printf("Hello from func:  processor %d, out of %d processors\n",
-               myTid, world_size);
+        send_to_tid(myTid, 1, myTid, 1, clock_d);
+
+        pthread_mutex_lock(&mutexClock);
+        clock_d++;
+        pthread_mutex_unlock(&mutexClock);
+
+        printf("Send: message %d\n",
+               myTid);
         sleep(5);
     }
 }
@@ -48,10 +82,15 @@ int main(int argc, char** argv) {
     pthread_t threadId;
     pthread_create(&threadId, &attr, send_loop, nullptr);
 
+    Packet* packet = new Packet();
+
+    recive(packet);
+
     // Print off a hello world message
-    printf("Hello from main:  processor %d, out of %d processors\n",
-           myTid, world_size);
+    printf("Hello from main:  processor %d, out of %d processors, recived: %d\n",
+           myTid, world_size, packet->message);
 
     // Finalize the MPI environment.
+    delete packet;
     MPI_Finalize();
 }
