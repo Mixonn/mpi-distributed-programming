501d7403034b5adfe2d5caec5a3a3aef80f7a0a8
Bartosz Osipiuk <osipiuk.bartosz@gmail.com>
2019-04-29 15:54:49 +0200

    Format code, fix mutex

--- src/pyrkon.cpp
+++ src/pyrkon.cpp
@@ -15,8 +15,8 @@
 #include "event.hpp"
 
 const int WORKSHOPS_COUNT = 10;
-const int WORKSHOPS_CAPABILITY = 2;
-const int PYRKON_CAPABILITY = 10;
+const int WORKSHOPS_CAPABILITY = 1;
+const int PYRKON_CAPABILITY = 2;
 
 const int REQUEST_GET_WS = 133;
 const int ACCEPT_GET_WS = 134;
@@ -38,7 +38,7 @@ int get_random_number(int min, int max) {
 
 pthread_mutex_t workshop_mutex[WORKSHOPS_COUNT+1];
 pthread_mutex_t mutexClock;
-sem_t semaphore;
+sem_t workshop_semaphore[WORKSHOPS_COUNT+1];
 
 int clock_d = 1;
 int my_tid;
@@ -49,7 +49,8 @@ std::map<int, bool> workshops_to_visit; //todo: in the future we can mark it as 
 void reset_workshops_to_visit() {
     workshops_to_visit.clear();
     workshops_to_visit.insert(std::make_pair(0, false));
-    int toVisit = get_random_number(1, 5);
+    int toVisit = 1;
+//    int toVisit = get_random_number(1, 5);
     while(toVisit--){
         int rand = get_random_number(1, WORKSHOPS_COUNT);
         while(workshops_to_visit.find(rand) != workshops_to_visit.end()){
@@ -82,11 +83,6 @@ Packet receive() {
 
     Packet packet(arr[0], arr[1], arr[2], arr[3]);
     Log::info(my_tid, clock_d, "Received " + packet.to_string());
-
-    pthread_mutex_lock(&mutexClock);
-    clock_d = std::max(clock_d, packet.clock_d) + 1;
-    pthread_mutex_unlock(&mutexClock);
-
     return packet;
 }
 
@@ -98,52 +94,52 @@ void send_to_tid(int receiver, int queue_id, int request_type, int sent_clk) {
     MPI_Isend(arr, 5, MPI_INT, receiver, receiver, MPI_COMM_WORLD, &req);
 
     Packet packet(sent_clk, queue_id, my_tid, request_type);
-    Log::info(my_tid, sent_clk, "Sent to " + std::to_string(receiver) + ": " + packet.to_string());
+    Log::info(my_tid, sent_clk, "\tSent to " + std::to_string(receiver) + ": " + packet.to_string());
 }
 
 void send_to_all(int queue_id, int request_type, int sent_clk) {
     for (int i = 0; i < world_size; i++) {
-        if(i == my_tid){
-            continue;
-        }
+        // we are sending request even to this same process because we need notify that queue changed
         send_to_tid(i, queue_id, request_type, sent_clk);
     }
 }
 
 void *send_loop(void *id) {
     while (true) {
-        pthread_mutex_lock(&workshop_mutex[0]); //add me to queue
-        Node node;
-        node.tid = my_tid;
-        node.clk = clock_d;
-        workshops[0].queue.put(&node);
-        pthread_mutex_unlock(&workshop_mutex[0]);
 
-        send_to_all(0, REQUEST_GET_WS, clock_d);
+        pthread_mutex_lock(&workshop_mutex[0]); //add me to queue
+        {
+            Node node;
+            node.tid = my_tid;
+            node.clk = clock_d;
+            workshops[0].queue.put(&node);
+        } pthread_mutex_unlock(&workshop_mutex[0]);
 
         pthread_mutex_lock(&mutexClock);
-        clock_d++;
-        pthread_mutex_unlock(&mutexClock);
+        {
+            send_to_all(0, REQUEST_GET_WS, clock_d);
+            clock_d++;
+        } pthread_mutex_unlock(&mutexClock);
 
-        sem_wait(&semaphore); //waiting for all responses
+        sem_wait(&workshop_semaphore[0]); //waiting for all responses
         Log::color_info(my_tid, clock_d, "Received all responses", ANSI_COLOR_MAGENTA);
 
-        reset_workshops_to_visit();
-        std::string drawn_workshops;
-        for (auto & it : workshops_to_visit)
-        {
-            drawn_workshops += std::to_string(it.first) + ", ";
-        }
-        Log::debug(my_tid, clock_d, "Drawn workshops: " + drawn_workshops);
-        pthread_mutex_lock(&mutexClock);
-        for (auto & it : workshops_to_visit)
-        {
-            send_to_all(it.first,REQUEST_GET_WS, clock_d);
-            clock_d++;
-        }
-        pthread_mutex_unlock(&mutexClock);
-
+//        reset_workshops_to_visit();
+//        std::string drawn_workshops;
+//        for (auto & it : workshops_to_visit)
+//        {
+//            drawn_workshops += std::to_string(it.first) + ", ";
+//        }
+//        Log::debug(my_tid, clock_d, "Drawn workshops: " + drawn_workshops);
+//        pthread_mutex_lock(&mutexClock);
+//        for (auto & it : workshops_to_visit)
+//        {
+//            send_to_all(it.first,REQUEST_GET_WS, clock_d);
+//            clock_d++;
+//        }
+//        pthread_mutex_unlock(&mutexClock);
         sleep(30);
+
     }
 }
 
@@ -162,11 +158,9 @@ int main(int argc, char **argv) {
     for(int i = 0; i <= WORKSHOPS_COUNT; i++){
         workshops[i].id = i;
         pthread_mutex_init(&workshop_mutex[i], nullptr);
+        sem_init(&workshop_semaphore[i], 0, 1);
+        sem_wait(&workshop_semaphore[i]);
     }
-
-
-    sem_init(&semaphore, 1, 1);
-    sem_wait(&semaphore);
 
     pthread_t threadId;
     pthread_create(&threadId, &attr, send_loop, nullptr);
@@ -174,29 +168,47 @@ int main(int argc, char **argv) {
     while (true) {
         Packet packet = receive();
         int const queue_id = packet.queue_id;
-        if(packet.request_type == REQUEST_GET_WS){
-            pthread_mutex_lock(&workshop_mutex[queue_id]);
-            Node *node = new Node;
-            node->tid = packet.tid;
-            node->clk = packet.clock_d;
-            workshops[queue_id].queue.put(node);
-            pthread_mutex_unlock(&workshop_mutex[queue_id]);
-
+        if(packet.tid != my_tid) {
             pthread_mutex_lock(&mutexClock);
-            send_to_tid(packet.tid, queue_id, ACCEPT_GET_WS, clock_d);
-            clock_d++;
-            pthread_mutex_unlock(&mutexClock);
+            {
+                clock_d = std::max(clock_d, packet.clock_d) + 1;
+            } pthread_mutex_unlock(&mutexClock);
+
+            if (packet.request_type == REQUEST_GET_WS) {
+                pthread_mutex_lock(&workshop_mutex[queue_id]);
+                {
+                    Node *node = new Node;
+                    node->tid = packet.tid;
+                    node->clk = packet.clock_d;
+                    workshops[queue_id].queue.put(node);
+                } pthread_mutex_unlock(&workshop_mutex[queue_id]);
+
+                pthread_mutex_lock(&mutexClock);
+                {
+                    send_to_tid(packet.tid, queue_id, ACCEPT_GET_WS, clock_d);
+                    clock_d++;
+                } pthread_mutex_unlock(&mutexClock);
+            }
+            if (packet.request_type == ACCEPT_GET_WS) {
+                pthread_mutex_lock(&workshop_mutex[queue_id]);
+                {
+                    workshops[queue_id].accepted_counter++;
+                } pthread_mutex_unlock(&workshop_mutex[queue_id]);
+            }
         }
-        if(packet.request_type == ACCEPT_GET_WS){
-            pthread_mutex_lock(&workshop_mutex[queue_id]);
-            workshops[queue_id].accepted_counter++;
-            pthread_mutex_unlock(&workshop_mutex[queue_id]);
-        }
-        int queueSize = workshops[queue_id].queue.get_size();
-        int queuePos = workshops[queue_id].queue.get_pos(my_tid);
-        int ahead_of = queueSize - queuePos - 1;
-        if(ahead_of >= world_size - PYRKON_CAPABILITY && workshops[queue_id].accepted_counter == world_size - 1){
-            sem_post(&semaphore);
+
+        int ahead_of;
+        int accepted_counter;
+        pthread_mutex_lock(&workshop_mutex[queue_id]);
+        {
+            int queueSize = workshops[queue_id].queue.get_size();
+            int queuePos = workshops[queue_id].queue.get_pos(my_tid);
+            ahead_of = queueSize - queuePos - 1;
+            accepted_counter = workshops[queue_id].accepted_counter;
+        } pthread_mutex_unlock(&workshop_mutex[queue_id]);
+
+        if(ahead_of >= world_size - PYRKON_CAPABILITY && accepted_counter == world_size - 1){
+            sem_post(&workshop_semaphore[queue_id]);
         }
     }
 
